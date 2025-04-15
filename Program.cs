b4ab1c457877146daf61392fcc149e17d49d0567
Soomin Song <song.soomin.418@outlook.com>
2025-04-15 10:56:05 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿/// Design a tip calculator : enter the bill total,
+/// tip % and display grand total after adding tip.
+
+Console.WriteLine("Tip Calculator");
+
+Console.Write("Bill ($): ");
+double price = Convert.ToDouble(Console.ReadLine());
+Console.Write($"Tip (%): ");
+double tip = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"\nTip: {(price*tip/100).ToString("C",new System.Globalization.CultureInfo("en-US"))}");
+Console.WriteLine($"Total: {(price+price*tip/100).ToString("C",new System.Globalization.CultureInfo("en-US"))}");
+
+
