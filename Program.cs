7defb11385861e5982e1c307f19d3845e41133d7
Soomin Song <song.soomin.418@outlook.com>
2025-04-15 14:30:55 -0700

    simplified

--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
 ﻿/// Design a tip calculator : enter the bill total,
 /// tip % and display grand total after adding tip.
 
+using System.Globalization;
+
 Console.WriteLine("Tip Calculator");
 
 Console.Write("Bill ($): ");
 double price = Convert.ToDouble(Console.ReadLine());
 Console.Write($"Tip (%): ");
-double tip = Convert.ToDouble(Console.ReadLine());
+double tip = Convert.ToDouble(Console.ReadLine())/100;
 
-Console.WriteLine($"\nTip: {(price*tip/100).ToString("C",new System.Globalization.CultureInfo("en-US"))}");
-Console.WriteLine($"Total: {(price+price*tip/100).ToString("C",new System.Globalization.CultureInfo("en-US"))}");
-
-
+Console.WriteLine($"\nBill: {price.ToString("C",CultureInfo.CurrentCulture)}");
+Console.WriteLine($"Tip ({tip:P}): {(price*tip).ToString("C",CultureInfo.CurrentCulture)}");
+Console.WriteLine($"Total: {(price+price*tip).ToString("C",CultureInfo.CurrentCulture)}");
